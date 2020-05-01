8c72807904af77baa7d042c314c7448386b086a7
Focusbe <focusbe@outlook.com>
2020-05-01 23:12:21 +0800

    添加firebase测试用例

---
+++ firebase/quickstart-js/init.js
@@ -0,0 +1,12 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyBKuwH-zoVMCTmT3NupvGzvkAGPYS7VgtM",
+    authDomain: "nore-6f6c7.firebaseapp.com",
+    databaseURL: "https://nore-6f6c7.firebaseio.com",
+    projectId: "nore-6f6c7",
+    storageBucket: "nore-6f6c7.appspot.com",
+    messagingSenderId: "886379825287",
+    appId: "1:886379825287:web:0c976ee188fa8415"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
