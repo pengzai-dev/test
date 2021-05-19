f18fe86348c2fbd8422890ec6e1d220c6f95a7b2
pengzai <roc.liu@hotmail.com>
2021-05-19 19:44:39 +0800

    测试pdfjs

--- react-playground/src/demos/Home/index.jsx
+++ react-playground/src/demos/Home/index.jsx
@@ -1,3 +1,8 @@
+import { Document,Page } from 'react-pdf/dist/esm/entry.webpack';
 export default function Home() {
-    return <div>HOME</div>
+    return (
+        <Document renderMode="svg" file="1.pdf">
+            <Page pageNumber={1} />
+        </Document>
+    );
 }
